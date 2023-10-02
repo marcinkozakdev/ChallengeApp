cdd531e135ee4244994431b4308fc06b318b8c8f
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-10-02 21:09:47 +0200

    Task - Day 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Zadanie domowe - dzień 4
+
+string name = "Ewa";
+char sex = 'K';
+int age = 29;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (sex == 'M' && age < 18 )
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
