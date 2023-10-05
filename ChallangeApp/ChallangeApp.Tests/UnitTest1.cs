9ae1c25556a46d5cc01980646ab36709d7e4b320
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-10-05 23:54:27 +0200

    Task - Day 7

---
+++ ChallangeApp/ChallangeApp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System.Reflection.Metadata;
+
+namespace ChallangeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenTwoNumberAreAdded_ShouldReturnSum()
+        {
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+    }
+}
