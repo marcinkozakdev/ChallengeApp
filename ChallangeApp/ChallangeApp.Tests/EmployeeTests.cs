77d70ec2301856b7faceecae2513e452424c0549
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-10-07 13:26:13 +0200

    Task - Day 7

---
+++ ChallangeApp/ChallangeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,37 @@
+namespace ChallangeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenEmployeeCollectTwoScores_ShouldCorrectResult()
+        {
+            // arrange
+            var employee = new Employee("Name","Surname",30);
+            employee.AddScore(5);
+            employee.AddScore(6);
+
+            // act
+            var result = employee.Result;
+
+            // assert
+            Assert.AreEqual(11, result);
+        }
+
+        [Test]
+        public void WhenEmployeeCollectDifferentScores_ShouldCorrectResult()
+        {
+            var employee = new Employee("Name", "Surname", 30);
+            employee.AddScore(5);
+            employee.AddScore(-2);
+            employee.AddScore(0);
+            employee.AddScore(10);
+            employee.AddScore(-6);
+
+            // act
+            var result = employee.Result;
+
+            // assert
+            Assert.AreEqual(7, result);
+        }
+    }
+}
