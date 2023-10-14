b1003229e849556c971a33d06173e27354162504
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-10-14 21:21:23 +0200

    Task - Day 12

--- ChallangeApp/ChallangeApp/Employee.cs
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -85,23 +85,23 @@ namespace ChallangeApp
             {
                 case 'A':
                 case 'a':
-                    this.grades.Add(100);
+                    AddGrade(100);
                     break;
                 case 'B':
                 case 'b':
-                    this.grades.Add(80);
+                    AddGrade(80);
                     break;
                 case 'C':
                 case 'c':
-                    this.grades.Add(60);
+                    AddGrade(60);
                     break;
                 case 'D':
                 case 'd':
-                    this.grades.Add(40);
+                    AddGrade(40);
                     break;
                 case 'E':
                 case 'e':
-                    this.grades.Add(20);
+                    AddGrade(20);
                     break;
             }
         }
